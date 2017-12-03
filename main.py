f3d51e0dc75a2591d7e433a705cd3352762270f8
Youngbok Yoon <master@bluehack.net>
2017-12-03 16:01:06 +0900

    Changed saving version_name to version_id

--- main.py
+++ main.py
@@ -20,12 +20,13 @@ except ImportError:
 
 CWD_PATH = os.getcwd()
 
-REDIS_VERSION_CRAWL_QUEUE = 'bl:version:crawl:queue'
+REDIS_CRAWL_VERSION_QUEUE = 'bl:version:crawl:queue'
+REDIS_CRAWL_VERSION = 'bl:crawl:version'
+REDIS_CRAWL_VERSION_LATEST = 'latest'
 REDIS_HOST_CRAWL_QUEUE = 'bl:host:crawl:queue'
 
 REDIS_SERVER = os.environ['REDIS_SERVER']
 REDIS_PASSWORD = os.environ['REDIS_PASSWORD']
-SUBSCRIBE_TOPIC = os.environ['SUBSCRIBE_TOPIC']
 rconn = redis.StrictRedis(REDIS_SERVER, port=6379, password=REDIS_PASSWORD)
 
 options = {
@@ -35,7 +36,7 @@ options = {
 log = Logging(options, tag='bl-crawler')
 
 
-def spawn_crawler(version):
+def spawn_crawler():
 
   id = str(uuid.uuid4())
   pool = spawning_pool.SpawningPool()
@@ -57,7 +58,6 @@ def spawn_crawler(version):
   pool.addContainerEnv(container, 'REDIS_SERVER', REDIS_SERVER)
   pool.addContainerEnv(container, 'REDIS_PASSWORD', REDIS_PASSWORD)
   pool.addContainerEnv(container, 'SPAWN_ID', id)
-  pool.addContainerEnv(container, 'PRODUCT_VERSION', version)
   pool.setContainerImage(container, 'bluelens/bl-crawler:latest')
   pool.addContainer(container)
   pool.setRestartPolicy('Never')
@@ -72,20 +72,24 @@ def create_new_version(version_name):
   try:
     res = version_api.add_version(version)
     if res.data != None and res.data.version_id != None:
+      set_latest_crawl_version(res.data.version_id)
       return True
   except ApiException as e:
     log.error("Exception when calling VersionApi->add_version: %s\n" % e)
-    return False
 
   return False
 
 def push_host_to_queue(host_code):
   rconn.lpush(REDIS_HOST_CRAWL_QUEUE, host_code)
 
-def start_crawl(version):
+
+def set_latest_crawl_version(version_id):
+  rconn.hset(REDIS_CRAWL_VERSION, REDIS_CRAWL_VERSION_LATEST, version_id)
+
+def start_crawl(version_name):
   log.info('start_crawl')
+
   host_api = stylelens_product.HostApi()
-  host = Host()
 
   offset = 0
   limit = 100
@@ -94,7 +98,7 @@ def start_crawl(version):
       res = host_api.get_hosts(offset=offset, limit=limit)
       for h in res.data:
         push_host_to_queue(h.host_code)
-        spawn_crawler(version)
+        spawn_crawler()
 
       if limit > len(res.data):
         break
@@ -105,11 +109,11 @@ def start_crawl(version):
 
 def dispatch(rconn):
   while True:
-    key, value = rconn.blpop([REDIS_VERSION_CRAWL_QUEUE])
-    version = value.decode('utf-8')
-    ret = create_new_version(version)
+    key, value = rconn.blpop([REDIS_CRAWL_VERSION_QUEUE])
+    version_name = value.decode('utf-8')
+    ret = create_new_version(version_name)
     if ret == True:
-      start_crawl(version)
+      start_crawl(version_name)
 
 if __name__ == '__main__':
   log.info('Start bl-crawl')
